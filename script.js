f14dc9d76c297133f8823a9975f141df2352a2f1
Zhangtong <heliyunduan@qq.com>
2026-09-24 19:54:48 +0800

    2026.9.24文字实验室

--- script.js
+++
@@ -1,8 +0,0 @@
-    // JS：点击标题，文字颜色随机变化
-    
-    const title = document.getElementById('title');
-    title.addEventListener('click', function () {
-      const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0');
-      title.style.color = randomColor;
-
-    });
